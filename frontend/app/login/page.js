fbe0b64b370d64de6d0345c92c191f20d9afd8e8
ShrutiSutar23 <shruti.sutar231992@gmail.com>
2026-07-09 12:18:18 +0530

    Triying to fix frontend

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -13,7 +13,7 @@ export default function Login() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch("API_URL/api/login", {
+    fetch(API_URL + "/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email: email, password: password }),
